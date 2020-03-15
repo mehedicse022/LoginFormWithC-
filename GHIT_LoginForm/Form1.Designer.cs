c7ce238be9fe8c790c5defdd57b9782574184b0f
Mehedi Hasan <mehedi@ghit.com>
2020-03-15 15:40:48 +0600

    Login Form With C# Visual Studio

---
+++ GHIT_LoginForm/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace GHIT_LoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Location = new System.Drawing.Point(12, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 133);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(218, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(219, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(222, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password :";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(313, 75);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(128, 20);
+            this.username.TabIndex = 4;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(313, 110);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(128, 20);
+            this.password.TabIndex = 5;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // login
+            // 
+            this.login.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.login.Image = ((System.Drawing.Image)(resources.GetObject("login.Image")));
+            this.login.Location = new System.Drawing.Point(286, 187);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(95, 32);
+            this.login.TabIndex = 8;
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("cancel.BackgroundImage")));
+            this.cancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancel.Location = new System.Drawing.Point(379, 187);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(95, 32);
+            this.cancel.TabIndex = 9;
+            this.cancel.Text = "CANCEL";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(486, 247);
+            this.ControlBox = false;
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User Login Authentication";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Button cancel;
+    }
+}
+
---
+++ GHIT_LoginForm/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GHIT_LoginForm
+{
+
+   
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = WADEY; Initial Catalog = ghit; Integrated Security = True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                // Update users table status to '0' if cancel
+
+                string queryup = ("update users set status = '0' where status = '1' ");
+                SqlDataAdapter cancel = new SqlDataAdapter(queryup, con);
+                DataTable dtexit = new DataTable();
+                cancel.Fill(dtexit);
+                this.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //con.Open();
+
+                // Check Login username and password on users table
+
+                string query = ("select * from users where username='" + this.username.Text + "' and password ='" + this.password.Text + "' ");
+                SqlDataAdapter log = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                log.Fill(dt);
+                if (dt.Rows.Count == 1)
+                {
+                    // update users table status and set to 1 .. if successfull 
+
+                    string queryuplogin = ("update users set status='1' where status = '0' and username='" + this.username.Text + "' ");
+                    SqlDataAdapter upd = new SqlDataAdapter(queryuplogin, con);
+
+                    DataTable dtup = new DataTable();
+                    upd.Fill(dtup);
+
+                    // Show Message 
+
+                    MessageBox.Show("Welcome to Apllication !!!");
+                    Main window = new Main();
+                    window.Show();
+
+                }
+                else
+                {
+                    MessageBox.Show("Please Check Your Username and Password ");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
